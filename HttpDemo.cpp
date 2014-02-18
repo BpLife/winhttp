ed2ce53e3e2e8adcb77dc057a107b8671323a7d6
bplife <52715027@qq.com>
2014-02-18 13:48:10 +0800

    If you use http in application,well,the winhttp library suits for you .

---
+++ HttpDemo.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include"WinHttp.h"
+using namespace std;
+int main()
+{
+
+	
+	//默认Get方式
+	CWinHttp WinHttp;
+
+	WCHAR *url=L"http://www.baidu.com/";
+
+	//打开连接
+	if(!WinHttp.Open(url))
+		return 0;
+	//添加请求头选项
+	LPWSTR host=L"Host:www.baidu.com";
+	if(!WinHttp.AddRequestHeader(host,lstrlenW(host)))
+		return 0;
+	//发送请求
+	if(!WinHttp.SendRequest("hello",5))
+		return 0;
+	//得到返回内容
+	if(!WinHttp.GetResponseConext())
+		return 0;
+	// 转码
+	ConvertUtf8ToGBK(WinHttp.m_strResponseContent);
+	cout<<WinHttp.m_strResponseContent<<endl;
+
+	//得到请求头
+	WinHttp.GetAllHeaders();
+	wcout<<WinHttp.m_strAllHeaders<<endl;
+	getchar();
+}
